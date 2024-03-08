92e589d33daf0b596a844e736d1fe1733cd4a38b
Scott Teal <s.teal726@gmail.com>
2024-03-08 10:43:23 -0800

    Add files via upload

---
+++ demo.sql
@@ -0,0 +1,87 @@
+-- Create a separate database and warehouse for demo
+CREATE OR REPLACE DATABASE demo;
+CREATE OR REPLACE WAREHOUSE demo_wh;
+USE DATABASE demo;
+USE WAREHOUSE demo_wh;
+
+-- External Volume named s3_vol created in advance
+CREATE OR REPLACE EXTERNAL VOLUME s3_vol
+    STORAGE_LOCATIONS = 
+        (
+            (
+                NAME = '<your-name>'
+                STORAGE_PROVIDER = 'S3'
+                STORAGE_BASE_URL = 's3://<bucket>/'
+                STORAGE_AWS_ROLE_ARN = 'arn:aws:iam::<role-id>:role/<role-name>'
+            )
+        );
+
+-- Create a Snowflake-managed Iceberg table that writes to Amazon S3
+CREATE OR REPLACE ICEBERG TABLE demo.public.product_reviews (
+    id STRING,
+    product_name STRING,
+    product_id STRING,
+    reviewer_name STRING,
+    review_date DATE,
+    review STRING
+)
+    CATALOG = 'SNOWFLAKE'
+    EXTERNAL_VOLUME = 's3_vol'
+    BASE_LOCATION = 'demo/product_reviews/'
+;
+SELECT * FROM demo.public.product_reviews LIMIT 10;
+
+-- Create a file format to specify how CSVs should be parsed
+CREATE OR REPLACE FILE FORMAT csv_ff
+    TYPE = 'CSV'
+    FIELD_OPTIONALLY_ENCLOSED_BY = '"'
+    SKIP_HEADER = 1;
+
+-- Create a stage to store the CSV files
+CREATE OR REPLACE STAGE demo.public.files
+    FILE_FORMAT = csv_ff
+    DIRECTORY = (ENABLED = TRUE);
+
+-- Create a stream to capture new records in the Iceberg table
+CREATE STREAM product_reviews_stream ON TABLE product_reviews;
+
+-- Create task to process new records with Cortex sentiment LLM function
+CREATE OR REPLACE TASK cortex_sentiment_score
+    SCHEDULE = 'USING CRON 0 0 * * * America/Los_Angeles'
+    USER_TASK_MANAGED_INITIAL_WAREHOUSE_SIZE = 'XSMALL'
+AS
+UPDATE demo.public.product_reviews pr
+SET sentiment = stream_sentiment
+FROM (
+    SELECT
+        id,
+        snowflake.cortex.sentiment(review) AS stream_sentiment
+    FROM demo.public.product_reviews
+) s
+WHERE pr.id = s.id;
+
+-- Create a secure view to be shared
+CREATE OR REPLACE SECURE VIEW demo.public.reviews_per_day
+AS
+SELECT
+    review_date,
+    COUNT(DISTINCT id) AS num_reviews
+FROM demo.public.product_reviews
+GROUP BY 1;
+
+-- Create a secure view to be shared
+CREATE OR REPLACE SECURE VIEW demo.public.product_sentiment
+AS
+SELECT
+    product_name,
+    AVG(sentiment) AS avg_sentiment
+FROM demo.public.product_reviews
+GROUP BY 1;
+
+-- Use UI to create a reader account
+-- Use UI to create a share with reader account and add both secure views to share
+
+
+--Reload
+DELETE FROM demo.public.product_reviews
+    WHERE REVIEW_DATE >= DATE('2024-02-01');
